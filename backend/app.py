8627fa839fa242149491b5cbf152f3d26207168e
Trev241 <trevisliu@gmail.com>
2023-02-03 02:58:22 +0530

    Revert "API overhauled"
    This reverts commit aa70ec3f6d19429fcb1524db2ddb8049d87d9e4b.

--- backend/app.py
+++ backend/app.py
@@ -1,115 +1,51 @@
-import os
+import json
 import pandas as pd
 
-from flask import Flask, jsonify
-from flask_cors import CORS
-from flask_sqlalchemy import SQLAlchemy
-from flask_marshmallow import Marshmallow
-from sqlalchemy.orm import relationship
+from flask import Flask
+from flask_restful import Api, Resource
+from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
-cors = CORS(
-    app, 
-    resources={
-        r'*': {'origins': '*'}
-    }
-)
-basedir = os.path.abspath(os.path.dirname(__file__))
-app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{os.path.join(basedir, "db.sqlite")}'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy(app)
-ma = Marshmallow(app)
+cors = CORS(app, resources={r"*": {"origins": "*"}})
+api = Api(app)
 
-class Country(db.Model):
-    __tablename__ = 'country'
+# ----------------------------------
+# Reading data from CSV file
+# TODO: Maybe change to SQL database 
+# ----------------------------------
+RESOURCES = {
+    'INCOME_INDEX': 'income_index.csv', 
+    'CAPITAL': 'capital_formation.csv', 
+    'DOMESTIC_CREDITS': 'domestic_credits.csv', 
+    'GDP_PER_CAPITA': 'gdp_per_capita.csv', 
+    'GDP': 'gdp.csv', 
+    'GNI_FEMALE': 'gni_female.csv', 
+    'GNI MALE': 'gni_male.csv', 
+    'GNI_PER_CAPITA': 'gni_per_capita.csv', 
+    'LABOUR_SHARE': 'labour_share.csv'
+}
 
-    id = db.Column(db.Integer, primary_key=True)
-    un_code = db.Column(db.Integer, unique=True, nullable=False)
-    iso_alpha_2_code = db.Column(db.String(2), unique=True, nullable=False)
-    iso_alpha_3_code = db.Column(db.String(3), unique=True, nullable=False)
-    name = db.Column(db.String(32), nullable=False)
+DATA = dict()
 
-    def __init__(self, un_code, iso_alpha_2_code, iso_alpha_3_code, name):
-        self.un_code = un_code
-        self.iso_alpha_2_code = iso_alpha_2_code
-        self.iso_alpha_3_code = iso_alpha_3_code
-        self.name = name
+for name, resource in RESOURCES.items():
+    print(f'Reading {name} from resource: {resource}...')
+    
+    data = pd.read_csv(f'data/{resource}')
+    data.set_index(['Country'], inplace=True)
 
-class CountrySchema(ma.Schema):
-    class Meta:
-        fields = ('id', 'un_code', 'iso_alpha_2_code', 'iso_alpha_3_code', 'name')
+    DATA[name] = data
 
-country_schema = CountrySchema()
-countries_schema = CountrySchema(many=True)
+class Country(Resource):
+    def get(self, country_code):
+        response = dict()
+        
+        for resource_name in RESOURCES.keys():
+            # TODO: Ignore favicon.ico GET requests
+            response[resource_name] = DATA[resource_name].loc[country_code].to_json()
+        
+        return json.dumps(response)
 
-@app.route('/country', methods=['GET'])
-def get_countries():
-    countries = Country.query.all()
-    result = countries_schema.dump(countries)
-    return jsonify(result)
-
-@app.route('/country/<iso_alpha_3_code>', methods=['GET'])
-def get_country(iso_alpha_3_code: str):
-    country = Country.query.filter_by(iso_alpha_3_code=iso_alpha_3_code.upper()).one()
-    return country_schema.jsonify(country)
-
-class EconomicIndicator(db.Model):
-    __tablename__ = 'economic_indicator'
-
-    id = db.Column(db.Integer, primary_key=True)
-    short_name = db.Column(db.String(16), unique=True, nullable=False)
-    name = db.Column(db.String(32), nullable=False)
-    description = db.Column(db.String(128), nullable=False)
-
-    def __init__(self, short_name, name, description):
-        self.short_name = short_name
-        self.name = name
-        self.description = description
-
-class EconomicIndicatorSchema(ma.Schema):
-    class Meta:
-        fields = ('id', 'short_name',  'name', 'description')
-
-eco_indicator_schema = EconomicIndicatorSchema()
-eco_indicators_schema = EconomicIndicatorSchema(many=True)
-
-@app.route('/indicator', methods=['GET'])
-def get_indicators():
-    indicators = EconomicIndicator.query.all()
-    result = eco_indicators_schema.dump(indicators)
-    return jsonify(result)
-
-class CountryIndicatorValue(db.Model):
-    __tablename__ = 'country_indicator_value'
-
-    id = db.Column(db.Integer, primary_key=True)
-    country_id = db.Column(db.Integer, db.ForeignKey('country.id'), nullable=False)
-    indicator_id = db.Column(db.Integer, db.ForeignKey('economic_indicator.id'), nullable=False)
-    year = db.Column(db.Integer, nullable=False)
-    value = db.Column(db.Float, nullable=False)
-
-    country = db.relationship('Country', backref=db.backref('values', lazy=True))
-    indicator = db.relationship('EconomicIndicator', backref=db.backref('values', lazy=True))
-
-    def __init__(self, country_id, indicator_id, year, value):
-        self.country_id = country_id
-        self.indicator_id = indicator_id
-        self.year = year
-        self.value = value
-
-class CountryIndicatorValueSchema(ma.Schema):
-    class Meta:
-        fields = ('id', 'country_id', 'indicator_id', 'year', 'value')
-
-country_val_schema = CountryIndicatorValueSchema()
-country_vals_schema = CountryIndicatorValueSchema(many=True)
-
-@app.route('/value/<iso_alpha_3_code>', methods=['GET'])
-def get_values(iso_alpha_3_code: str):
-    id = Country.query.filter_by(iso_alpha_3_code=iso_alpha_3_code).one().id
-    values = CountryIndicatorValue.query.filter_by(country_id=id).all()
-    result = country_vals_schema.dump(values)
-    return jsonify(result)
+api.add_resource(Country, '/<string:country_code>')
 
 if __name__ == '__main__':
     app.run(debug=True)
