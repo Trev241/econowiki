cd62edf465d03a287eb99725e490838222a5032d
Naveensb7 <naveensb140@gmail.com>
2023-04-20 13:02:42 +0530

    Fix error in API /stats route

--- backend/routes/misc.py
+++ backend/routes/misc.py
@@ -13,7 +13,7 @@ def get_stats():
         'country_count': len(Country.query.all()),
         'indicator_count': len(EconomicIndicator.query.all()),
         'value_count': len(CountryIndicatorValue.query.all()),
-        'user_count': len(User.query.filter_by(accepted=True))
+        'user_count': len(User.query.filter_by(accepted=True).all())
     }
 
     return jsonify(stats), 200
